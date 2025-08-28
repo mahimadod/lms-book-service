6d5f0079349de06accf5e7f96595ecb46c3b89ca
mahima <dodmahima@gmail.com>
2025-08-28 22:42:13 +0530

    initial commit

--- src/main/java/com/example/book/controller/AuthorController.java
+++ src/main/java/com/example/book/controller/AuthorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("book-service/api/authors")
+@RequestMapping("api/authors")
 @CrossOrigin
 public class AuthorController {
 
--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
-@RequestMapping("book-service/api/books")
+@RequestMapping("api/books")
 @CrossOrigin
 public class BookController {
 
